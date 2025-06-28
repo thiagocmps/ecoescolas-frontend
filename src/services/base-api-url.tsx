da48d4de359c6d4bae3c6b388324ae6e5e2d077d
Thiago Campos Vidal de Souza <thiagocvs2003@gmail.com>
2025-06-28 11:25:50 +0100

    feat: alterar configuração de localhost para produção
    Alteração em assets: Mudança de ícones

--- src/services/base-api-url.tsx
+++ src/services/base-api-url.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_BASE_URL } from "../../config";
 
-const isLocalhost = true; // Defina como true se estiver em localhost
+const isLocalhost = false; // Defina como true se estiver em localhost
 const baseUrl = !isLocalhost
   ? "https://ecoescolas-backend.onrender.com"
   : /* "http://localhost:5000" */ "http://192.168.1.217:5000";
